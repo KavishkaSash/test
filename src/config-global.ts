084465216e0706a5b9fcd481f4ca99791e643db7
KavishkaSash <kavishka@teatang.com>
2025-02-17 09:27:50 +0530

    update appName in configuration to 'ITP 9.0 TEST'

--- src/config-global.ts
+++ src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'ITP 9.0 TEST',
   appVersion: packageJson.version,
 };
